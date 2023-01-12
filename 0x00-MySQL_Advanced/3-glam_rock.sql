d696193e1da143e51992ca9db7b89bf43f2a2491
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-12 14:37:28 +0100

    COALESCE, YEAR, NOW functions and LIKE pattern matching

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- ranks Glam rock style bands by their longevity
-SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan
+SELECT band_name, (COALESCE(split, YEAR(NOW())) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
