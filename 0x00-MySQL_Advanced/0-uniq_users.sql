baf1c2dc688c6aa92d44ef99538ce4ae40c5e3ec
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-12 06:10:20 +0100

    task 0: create users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a users table
+CREATE TABLE users IF NOT EXISTS (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+)
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,1 @@
+### some advanced MySQL sytaxes
