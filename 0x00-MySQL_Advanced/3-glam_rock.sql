fdfae52f411d307ed4608e92bfc1d9d7f8eddaf9
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-13 13:14:45 +0100

    pretty formating

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
 -- ranks Glam rock style bands by their longevity
-SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
+SELECT
+  band_name,
+  (COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
