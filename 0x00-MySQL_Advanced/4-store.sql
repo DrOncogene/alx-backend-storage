9e2704fd9adbd4c7d6660143258240196da8f871
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-13 19:46:44 +0100

    mysql triggers

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- creates a trigger to decrease items quantity
+-- when an order is place
+DELIMITER **
+CREATE TRIGGER update_quatity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+  UPDATE TABLE items
+  SET quantity = quantity - NEW.number
+  WHERE name = NEW.item_name;
+END**
+
+DELIMITER ;
