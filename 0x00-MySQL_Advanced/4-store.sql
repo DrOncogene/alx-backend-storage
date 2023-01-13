ffaa5f2356bd93d891427da83736955b6d21b1b9
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-13 19:57:59 +0100

    mysql triggers

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER update_quatity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-  UPDATE TABLE items
+  UPDATE items
   SET quantity = quantity - NEW.number
   WHERE name = NEW.item_name;
 END**
