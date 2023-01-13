6436e9dc71c45358a804294feba98114e3631aa3
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-13 12:26:33 +0100

    changed the current year to 2022

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- ranks Glam rock style bands by their longevity
-SELECT band_name, (COALESCE(split, YEAR(NOW())) - formed) AS lifespan
+SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
