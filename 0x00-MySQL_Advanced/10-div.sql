a922d4b4515e633dd29a98879f567815157f26d3
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-15 17:19:18 +0100

    stored function

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- function for save division
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+  IF b = 0 THEN
+    RETURN 0;
+  ELSE
+    RETURN a / b;
+  END IF;
+END $$
+
+DELIMITER $$
