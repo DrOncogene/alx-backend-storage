28d1be1a6ba6b555633fc3d8f654f3e49311be18
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-19 07:27:24 +0100

    pymongo insert_one;

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,5 +6,5 @@ insert a new document
 
 def insert_school(mongo_coll, **kwargs):
     """inserts a new document"""
-    new_id = mongo_call.insert_one(kwargs).inserted_id
+    new_id = mongo_coll.insert_one(kwargs).inserted_id
     return str(new_id)
