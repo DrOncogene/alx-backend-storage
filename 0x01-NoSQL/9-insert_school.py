0ff3f0098c8a8be3a692b3ed8b118d3f207c2e66
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-28 21:37:11 +0100

    fixed some errors

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ insert a new document
 def insert_school(mongo_coll, **kwargs):
     """inserts a new document"""
     new_id = mongo_coll.insert_one(kwargs).inserted_id
-    return str(new_id)
+    return new_id
