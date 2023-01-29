76f7fda4668798e2ec9c596439ffc8ae2e28ba7f
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-29 14:52:38 +0100

    parsing nginx logs stored in mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,18 +3,19 @@
 computes log stats
 """
 from pymongo import MongoClient
+from pymongo.collection import Collection
 
 
 client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_logs = client.logs.nginx
+nginx_logs: Collection = client.logs.nginx
 
-all_entries = len(nginx_logs.find())
-n_get = len(nginx_logs.find({'method': 'GET'}))
-n_post = len(nginx_logs.find({'method': 'POST'}))
-n_put = len(nginx_logs.find({'method': 'PUT'}))
-n_patch = len(nginx_logs.find({'method': 'PATCH'}))
-n_delete = len(nginx_logs.find({'method': 'DELETE'}))
-n_statuses = len(nginx_logs.find({'method': 'GET', 'path': '/status'}))
+all_entries = len(list(nginx_logs.find()))
+n_get = len(list(nginx_logs.find({'method': 'GET'})))
+n_post = len(list(nginx_logs.find({'method': 'POST'})))
+n_put = len(list(nginx_logs.find({'method': 'PUT'})))
+n_patch = len(list(nginx_logs.find({'method': 'PATCH'})))
+n_delete = len(list(nginx_logs.find({'method': 'DELETE'})))
+n_statuses = len(list(nginx_logs.find({'method': 'GET', 'path': '/status'})))
 
 print(f'{all_entries} logs')
 print('Methods:')
