a57f582c25c7695ee48b069d4695e7a62d031841
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-29 06:36:37 +0100

    updates a document if exists or create a new one

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,8 +6,4 @@ updates a record
 
 def update_topics(mongo_coll, name, topics):
     """updates a mongo document"""
-    match = mongo_coll.find_one({"name": name})
-    if match is None:
-        mongo_coll.insert_one({'name': name, 'topics': topics})
-    else:
-        mongo_coll.update_one({"name": name}, {'$set': {"topics": topics}})
+    mongo_coll.update_one({"name": name}, {'$set': {"topics": topics}})
