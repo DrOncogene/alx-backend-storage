e4a42964f60fc844ca8d0c6ece05240ee593a9be
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-29 22:46:56 +0100

    parsing nginx logs stored in mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,29 +6,30 @@ saved in a mongodb
 from pymongo import MongoClient
 
 
-# connect to the db
-client = MongoClient('mongodb://127.0.0.1:27017')
-# get the collection reference
-nginx_logs = client.logs.nginx
+if __name__ == "__main__":
+    # connect to the db
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    # get the collection reference
+    nginx_logs = client.logs.nginx
 
-# number of documents
-all_entries = len(list(nginx_logs.find()))
-# number of GET requests
-n_get = len(list(nginx_logs.find({'method': 'GET'})))
-# POST requests
-n_post = len(list(nginx_logs.find({'method': 'POST'})))
-# PUT requests
-n_put = len(list(nginx_logs.find({'method': 'PUT'})))
-n_patch = len(list(nginx_logs.find({'method': 'PATCH'})))
-n_delete = len(list(nginx_logs.find({'method': 'DELETE'})))
-# number of status checks
-n_statuses = len(list(nginx_logs.find({'method': 'GET', 'path': '/status'})))
+    # number of documents
+    all_entries = len(list(nginx_logs.find()))
+    # number of GET requests
+    n_get = len(list(nginx_logs.find({'method': 'GET'})))
+    # POST requests
+    n_post = len(list(nginx_logs.find({'method': 'POST'})))
+    # PUT requests
+    n_put = len(list(nginx_logs.find({'method': 'PUT'})))
+    n_patch = len(list(nginx_logs.find({'method': 'PATCH'})))
+    n_delete = len(list(nginx_logs.find({'method': 'DELETE'})))
+    # number of status checks
+    n_statuses = len(list(nginx_logs.find({'method': 'GET', 'path': '/status'})))
 
-print(f'{all_entries} logs')
-print('Methods:')
-print(f'\tmethod GET: {n_get}')
-print(f'\tmethod POST: {n_post}')
-print(f'\tmethod PUT: {n_put}')
-print(f'\tmethod PATCH: {n_patch}')
-print(f'\tmethod DELETE: {n_delete}')
-print(f'{n_statuses} status check')
+    print(f'{all_entries} logs')
+    print('Methods:')
+    print(f'\tmethod GET: {n_get}')
+    print(f'\tmethod POST: {n_post}')
+    print(f'\tmethod PUT: {n_put}')
+    print(f'\tmethod PATCH: {n_patch}')
+    print(f'\tmethod DELETE: {n_delete}')
+    print(f'{n_statuses} status check')
