c355d3e0053ae26dd680a75e0829257fe95acb6b
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-01-30 15:29:34 +0100

    aggregation with $set, $avg and $sort

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -7,6 +7,7 @@ aggregating results
 def top_students(mongo_coll):
     """sort by average scores"""
     match = mongo_coll.aggregate([
-      {'$set': {'averageScore': {'$avg': '$topics.score'}}}
+      {'$set': {'averageScore': {'$avg': '$topics.score'}}},
+      {'$sort': {'averageScore': -1}}
     ])
     return match
