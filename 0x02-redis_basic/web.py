7eb4fa9059973d2407ddf13f821fcf5a34bb474d
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-02-04 11:25:51 +0100

    fetches and cache a web page

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,8 +16,7 @@ def get_page(url: str) -> str:
     fetch and cache pages
     """
     res = requests.get(url, timeout=1000)
-    db.set(url, res.text)
-    db.expire(url, timedelta(seconds=10.0))
+    db.setex(url, timedelta(seconds=10.0), res.text)
     db.incr(f'count:{url}', 1)
 
     return res.text
